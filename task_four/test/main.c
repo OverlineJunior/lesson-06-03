ae027abbfbc861c7fd92620c644c0b9f0dda14d7
Over <franciscosebastianyjunior@hotmail.com>
2023-03-07 16:36:55 -0300

    Test everything added so far

--- task_four/test/main.c
+++ task_four/test/main.c
@@ -2,7 +2,12 @@
 #include "../int_set.h"
 
 int main(void) {
-    
+    IntSet intset = intset_new();
+    intset = intset_push(intset, 1);
+    intset = intset_push(intset, 2);
+    intset = intset_push(intset, 3);
+
+    intset_display(intset);
 
     return 0;
 }
